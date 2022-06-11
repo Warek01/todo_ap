c3d66bb55052192a8c8a575a76c1786d9bd76790
Alexandru Dobrojan <alexandru.dobrojan@isa.utm.md>
2022-06-11 13:36:20 +0300

    Fixed dropdown animation of menu 2

--- src/components/AppHeader/Menu.tsx
+++ src/components/AppHeader/Menu.tsx
@@ -3,32 +3,32 @@ import classNames from "classnames";
 
 
 interface MenuProps {
-    hidden: boolean;
+	hidden: boolean;
 }
 
 const Menu: React.FC<MenuProps> = ({hidden}) => {
-    const menuRef = useRef<HTMLDivElement>(null);
+	const menuRef = useRef<HTMLDivElement>(null);
 
-    return (
-        <div
-            id={"menu"}
-            className={classNames("menu", {"collapsed": hidden})}
-            ref={menuRef}
-        >
-            <button>
-                Clear all
-            </button>
-            <button>
-                Show completed
-            </button>
-            <button>
-                Show important
-            </button>
-            <button>
-                Show active
-            </button>
-        </div>
-    );
-}
+	return (
+		<div
+			id={"menu"}
+			className={classNames("menu", {"collapsed": hidden})}
+			ref={menuRef}
+		>
+			<button>
+				Clear all
+			</button>
+			<button>
+				Show completed
+			</button>
+			<button>
+				Show important
+			</button>
+			<button>
+				Show active
+			</button>
+		</div>
+	);
+};
 
 export default Menu;
